92a842c2ffa1ab9cfc393d4e6f3ca8f3d805c01c
FlyingV111 <73441162+FlyingV111@users.noreply.github.com>
2024-02-29 18:31:32 +0100

    Add AuthService

---
+++ src/app/Components/CustomComponents/sa-button/sa-button.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'sa-button',
+  templateUrl: './sa-button.component.html',
+  styleUrl: './sa-button.component.css',
+  standalone: true
+})
+export class SaButtonComponent {
+}
